5fc51e06ba0e2e4448a2e232b930adb2565ed0cf
Noemi Perez <nperezg@hybridars.com>
2019-02-27 19:42:11 +0100

    BS-64 - Refactor Reviews and ReviewStatus tables and related code

--- src/Hybridars/BioSounds/Entity/TagReview.php
+++ src/Hybridars/BioSounds/Entity/TagReview.php
@@ -6,64 +6,85 @@ use Hybridars\BioSounds\Database\Database;
 
 class TagReview
 {
-	const TABLE_NAME = "Reviews";
-	const SOUND_TAG = "sound_tag";
-	const USER = "user";
-	const STATUS = "status";
-	const ANIMAL = "animal";
-	const COMMENTS = "comments";
-	const DATE = "date";
+	const TABLE_NAME = 'tag_review';
+	const TAG = 'tag_id';
+	const USER = 'user_id';
+	const STATUS = 'tag_review_status_id';
+	const SPECIES = 'species_id';
+	const COMMENTS = 'note';
+	const DATE = 'creation_date';
 
 	//TODO: Create ReviewStatus model
-	const STATUS_TABLE_NAME = "ReviewStatus";
-	const STATUS_NAME = "description";
+	const STATUS_TABLE_NAME = 'tag_review_status';
+	const STATUS_NAME = 'name';
 
-    public function getTagReviews($tagID){
-		$query = "SELECT " . User::FULL_NAME. ", " . Species::BINOMIAL . ", " . self::STATUS_NAME . ", " . self::DATE . " FROM " . self::TABLE_NAME .
-		         " LEFT JOIN " . Species::TABLE_NAME . " ON "  . Species::TABLE_NAME .  "." . Species::ID . " = " . self::ANIMAL .
-		         " LEFT JOIN " . User::TABLE_NAME . " ON " . User::TABLE_NAME . "." . User::ID . " = " . self::USER . 
-		         " LEFT JOIN " . self::STATUS_TABLE_NAME . " ON " . self::STATUS_TABLE_NAME . ".id = " . self::STATUS . " WHERE " . self::SOUND_TAG. "=:tagID ORDER BY " . self::DATE;
+    /**
+     * @param int $tagId
+     * @return array
+     * @throws \Exception
+     */
+    public function getListByTag(int $tagId): array
+    {
+		$query = 'SELECT user.name as reviewer, species.binomial, tag_review_status.name as status, tag_review.' .
+            self::DATE . ' FROM ' . self::TABLE_NAME . ' LEFT JOIN ' . Species::TABLE_NAME . ' ON '  .
+            Species::TABLE_NAME .  '.' . Species::ID . ' = tag_review.' . self::SPECIES .
+            ' LEFT JOIN ' . User::TABLE_NAME . ' ON ' . User::TABLE_NAME . '.' . User::ID .
+            ' = tag_review.' . self::USER . ' LEFT JOIN ' . self::STATUS_TABLE_NAME .
+            ' ON ' . self::STATUS_TABLE_NAME . '.tag_review_status_id = tag_review.' . self::STATUS .
+            ' WHERE ' . self::TAG . ' = :tagId ORDER BY tag_review.' . self::DATE;
 
-		$values[":tagID"] = $tagID;
 		Database::prepareQuery($query);
-		$result = Database::executeSelect($values);	
-		return $result;
+		return Database::executeSelect([':tagId'=> $tagId]);
 	}
-	
-	public function hasUserReviewed($userID, $tagID){
-		$query = "SELECT COUNT(*) as NumReviews FROM " . self::TABLE_NAME. " WHERE " . self::USER . "=:userID AND " . self::SOUND_TAG. "=:tagID";
-		$values = [":userID" => $userID, ":tagID" => $tagID];
-		Database::prepareQuery($query);
-		$result = Database::executeSelect($values);
-		if(!empty($result) && $result[0]["NumReviews"] > 0)	
-			return true;
-		return false;	
+
+    /**
+     * @param int $userId
+     * @param int $tagId
+     * @return bool
+     * @throws \Exception
+     */
+	public function hasUserReviewed(int $userId, int $tagId): bool
+    {
+		Database::prepareQuery(
+		    'SELECT COUNT(*) as countReviews FROM ' . self::TABLE_NAME.
+            ' WHERE ' . self::USER . '= :userId AND ' . self::TAG. '= :tagId'
+        );
+		$result = Database::executeSelect([':userId' => $userId, ':tagId' => $tagId]);
+		if (!empty($result) && $result[0]['countReviews'] > 0) {
+		    return true;
+        }
+		return false;
 	}
-	
-    public function insertTagReview($tagData){
-		if(empty($tagData))
-			return false;
-			
-		$fields = "( ";
-		$valuesNames = "( ";
-		$values = array();
+
+    /**
+     * @param array $tagData
+     * @return array|bool|int
+     * @throws \Exception
+     */
+    public function insert(array $tagData)
+    {
+		if (empty($tagData)) {
+		    return false;
+        }
+
+		$fields = '( ';
+		$valuesNames = '( ';
+		$values = [];
 		$i = 1;
-		foreach($tagData as $key => $value){
+		foreach ($tagData as $key => $value) {
 			$fields .= $key;
-			$valuesNames .= ":".$key;
-			$values[":".$key] = $value;
-			if($i < count($tagData)){
-				$fields .= ", ";
-				$valuesNames .= ", ";
+			$valuesNames .= ':'.$key;
+			$values[':'.$key] = $value;
+			if ($i < count($tagData)) {
+				$fields .= ', ';
+				$valuesNames .= ', ';
 			} else {
-				$fields .= " )";
-				$valuesNames .= " )";
+				$fields .= ' )';
+				$valuesNames .= ' )';
 			}
 			$i++;			
 		}
-		$query = "INSERT INTO " . self::TABLE_NAME. " $fields VALUES $valuesNames";
-		Database::prepareQuery($query);
-		$result = Database::executeInsert($values);	
-		return $result;
+		Database::prepareQuery('INSERT INTO ' . self::TABLE_NAME. " $fields VALUES $valuesNames");
+		return Database::executeInsert($values);
 	}
 }
