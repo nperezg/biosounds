a50337c540bba810113cf920ec6d18994f7c9629
Noemi Perez <nperezg@hybridars.com>
2019-02-27 14:20:30 +0100

    BS-60 - Refactor Roles table and related code

--- src/Hybridars/BioSounds/Controller/UserController.php
+++ src/Hybridars/BioSounds/Controller/UserController.php
@@ -9,7 +9,7 @@ use Hybridars\BioSounds\Utils\Utils;
 
 class UserController
 {
-	const DEFAULT_TAG_COLOR = "#FFFFFF";
+	const DEFAULT_TAG_COLOR = '#FFFFFF';
 
     protected $listTpl = 'usersList.phtml';
     protected $formTpl = 'userForm.phtml';
@@ -49,37 +49,37 @@ class UserController
      */
     public function save()
     {
-		if (!Auth::isUserAdmin()){
+		if (!Auth::isUserAdmin()) {
 			throw new \Exception(ERROR_NO_ADMIN); 
 		}
 		
-		if (isset($_POST["admin_pwd"])) {
-			$adminPwd = filter_var($_POST["admin_pwd"], FILTER_SANITIZE_STRING); 
+		if (isset($_POST['admin_pwd'])) {
+			$adminPwd = filter_var($_POST['admin_pwd'], FILTER_SANITIZE_STRING);
 			$bdAdminPwd = $this->user->getPasswordByUserId(Auth::getUserLoggedID());
 			if(Utils::checkPasswords($adminPwd, $bdAdminPwd))
-				unset($_POST["admin_pwd"]);
+				unset($_POST['admin_pwd']);
 			else
-				throw new \Exception("The administrator password is not correct."); 	
+				throw new \Exception('The administrator password is not correct.');
 		}
 		
 		$data = [];
 
 		foreach ($_POST as $key => $value) {
-			if (strpos($key, "_")) {
-				$type = substr($key, strpos($key, "_") + 1, strlen($key));
-				$key = substr($key, 0, strpos($key, "_"));
+			if (strrpos($key, '_')) {
+				$type = substr($key, strrpos($key, '_') + 1, strlen($key));
+				$key = substr($key, 0, strrpos($key, '_'));
 
 				switch ($type) {
-					case "email":
+					case 'email':
 						$data[$key] =  filter_var($value, FILTER_SANITIZE_EMAIL); 
 						break;
-					case "checkbox":
+					case 'checkbox':
 						$data[$key] =  filter_var($value, FILTER_SANITIZE_NUMBER_INT);
 						break;
-					case "select-one":
+					case 'select-one':
 						$data[$key] =  filter_var($value, FILTER_SANITIZE_NUMBER_INT); 
 						break;
-					case "password":
+					case 'password':
 						$password = filter_var($value, FILTER_SANITIZE_STRING);
 						$data[$key] = Utils::encodePasswordHash($password);
 						break;
@@ -96,7 +96,7 @@ class UserController
             return $this->user->updateUser($data);
         }
 		else if($this->user->insertUser($data) > 0) {
-            header("Location: " . APP_URL . "/admin/users");
+            header('Location: ' . APP_URL . '/admin/users');
             die();
 		}
 	}
@@ -123,11 +123,11 @@ class UserController
     {
 		if (Auth::isUserAdmin()){
 			$this->getUsersList();
-			$this->view->usernameLabel = User::NAME . "_text";
-			$this->view->userFullNameLabel = User::FULL_NAME . "_text";
-			$this->view->userEmailLabel = User::EMAIL . "_email";
-			$this->view->userRoleLabel = User::ROLE . "_select-one";
-			$this->view->userPwdLabel = User::PASSWORD . "_password";
+			$this->view->usernameLabel = User::NAME . '_text';
+			$this->view->userFullNameLabel = User::FULL_NAME . '_text';
+			$this->view->userEmailLabel = User::EMAIL . '_email';
+			$this->view->userRoleLabel = User::ROLE . '_select-one';
+			$this->view->userPwdLabel = User::PASSWORD . '_password';
 		}
 	}
 
@@ -144,18 +144,18 @@ class UserController
 		$roles = $rol->getRoles();
 		
 		//New User Form
-		$this->view->rolesList = "";
+		$this->view->rolesList = '';
 		
 		foreach($roles as $values){				
-			$this->view->rolesList .= "<option value='" . $values["ID"] . "'>" . $values["Name"] . "</option>";
+			$this->view->rolesList .= "<option value='" . $values['role_id'] . "'>" . $values['name'] . '</option>';
 		}
 		//
 		
-		$this->view->listUsers = "";
+		$this->view->listUsers = '';
 		
 		foreach($listUsers as $userData) {
-			$disabled = "";
-			$privHidden = "";
+			$disabled = '';
+			$privHidden = '';
 			$userName = $userData[User::NAME];
 			$userFullName = $userData[User::FULL_NAME];
 			$userID = $userData[User::ID];
@@ -167,30 +167,30 @@ class UserController
 			$this->view->listUsers .= "<tr><td><input type='text' name='" . User::FULL_NAME . "' value='$userFullName'><input type='hidden' name='itemID' value='$userID'></td><td>$userName</td>";
 			$this->view->listUsers .= "<td><input type='email' name='" . User::EMAIL . "' value='$userEmail'></td>";
 			
-			$this->view->listUsers .= "<td>";	
+			$this->view->listUsers .= '<td>';
 			$isAdmin = $rol->isRoleAdmin($userRole);		
 
 			if ($currentUserID == $userID || ($isAdmin && ($this->user->countOtherAdminUsers($userID) == 0)))
-				$disabled = "disabled";
+				$disabled = 'disabled';
 				
 			$this->view->listUsers .= "<select $disabled name='" . User::ROLE . "'>";
 			foreach($roles as $values){				
-				$this->view->listUsers .= "<option value='" . $values["ID"] . "' " . ($userRole == $values["ID"] ? "selected" : "") . ">" . $values["Name"] . "</option>";
+				$this->view->listUsers .= "<option value='" . $values['role_id'] . "' " . ($userRole == $values['role_id'] ? "selected" : "") . ">" . $values["name"] . "</option>";
 			}
 
-			$this->view->listUsers .= "</select></td>";
+			$this->view->listUsers .= '</select></td>';
 			$this->view->listUsers .= "<td><input name='" . User::ACTIVE . "' type='checkbox' " . ($userActive ?  "checked" : "") . " $disabled></td>";
 			
 			if($isAdmin)
-				$privHidden = "hidden";
+				$privHidden = 'hidden';
 
 			$this->view->listUsers .= "<td><a href='" . APP_URL . "/ajaxcallmanager.php?class=user&action=show&id=$userID' class='open-modal' title='Edit Password'><span class='glyphicon glyphicon-pencil'></span></a></td>";
 			
 			$this->view->listUsers .= "<td><a href='" . APP_URL . "/ajaxcallmanager.php?class=UserPermission&action=manage&id=$userID' class='open-modal' title='Collection Privileges' $privHidden><span class='glyphicon glyphicon-tasks'></span></a></td>";
 
-            $this->view->listUsers .= '<td><input type="color" name="color" alt= "User tags color" value=' . $userTagColor . '></td>';;
+            $this->view->listUsers .= '<td><input type="color" name="color" alt= "Tag color" value=' . $userTagColor . '></td>';;
 						
-			$this->view->listUsers .= "</tr>";
+			$this->view->listUsers .= '</tr>';
 		}
 	}
 }
--- src/Hybridars/BioSounds/Entity/Role.php
+++ src/Hybridars/BioSounds/Entity/Role.php
@@ -4,21 +4,29 @@ namespace Hybridars\BioSounds\Entity;
 
 use Hybridars\BioSounds\Database\Database;
 
-class Role {
+class Role
+{
 
-	const ADMIN_ROLE = "Administrator";	
+	const ADMIN_ROLE = 'Administrator';
 	const ADMIN_ID = 1;
 	
-	public function __construct(){
-	}
-	
-	public function getRoles(){
-		Database::prepareQuery("SELECT * FROM Roles ORDER BY ID");
+    /**
+     * @return array|int
+     * @throws \Exception
+     */
+	public function getRoles()
+    {
+		Database::prepareQuery('SELECT * FROM role ORDER BY role_id');
 		$result = Database::executeSelect();
 		return $result;
 	}
-	
-	public function isRoleAdmin($role){
+
+    /**
+     * @param $role
+     * @return bool
+     */
+	public function isRoleAdmin($role)
+    {
 		return $role == self::ADMIN_ID ? true : false;
 	}
 }
