e161f38ce6323f7d57cc2c71cf1ebeacb0cd415d
pengww <wangpw0103@gmail.com>
2021-10-09 21:27:17 +0800

    temporarily comparing

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ docker-compose exec apache bash -c '
 while ! (nc -z queue 5672); do
   echo "Queue is not ready. Waiting...";
   sleep 2;
-done;
+done;'
 
-echo "Queue worker started."'
+echo "Queue worker started."
 
 docker-compose exec -T -u www-data apache nohup php worker.php > files_update.log 2>&1 &
